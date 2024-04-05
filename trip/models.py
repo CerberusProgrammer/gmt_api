2bcb8a362ce4a693980fe359dd1a62b7d5866bf2
Omar Flores <cerberusprogrammer@gmail.com>
2024-04-05 12:47:59 -0700

    New model for trip

--- trip/models.py
+++ trip/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class Trip(models.Model):
+    CITIES = (
+        ("MXL", "Mexicali"),
+        ("TCT", "Tecate"),
+        ("TIJ", "Tijuana"),
+        ("RST", "Rosarito"),
+        ("ENS", "Ensenada"),
+    )
+    
+    VEHICLES = (
+        ()
+    )
+    
+    from_city = models.CharField(max_length=20, choices=CITIES)
+    from_city_date = models.DateTimeField()
+    to_city = models.CharField(max_length=20, choices=CITIES)
+    to_city_data = models.DateTimeField()
+    passengers = models.IntegerField()
+    vehicle = models.CharField(max_length=100, choices=VEHICLES)
+    toll = models.DecimalField()
+    
