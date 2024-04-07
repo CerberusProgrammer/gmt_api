da7a83d139397e153f0bb863cd83f3906ef23a9d
sazarcode <cerberusprogrammer@gmail.com>
2024-04-06 21:42:47 -0700

    Actualizacion de rutas

--- trip/urls.py
+++ trip/urls.py
@@ -5,10 +5,10 @@ from rest_framework.routers import DefaultRouter
 from trip.views import CasetaViewSet, RutaViewSet, VehiculoViewSet, ViajeViewSet
 
 router = DefaultRouter()
-router.register(r'viajes', ViajeViewSet)
-router.register(r'rutas', RutaViewSet)
-router.register(r'casetas', CasetaViewSet)
-router.register(r'vehiculos', VehiculoViewSet)
+router.register(r'trips', ViajeViewSet)
+router.register(r'routes', RutaViewSet)
+router.register(r'stands', CasetaViewSet)
+router.register(r'vehicles', VehiculoViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
